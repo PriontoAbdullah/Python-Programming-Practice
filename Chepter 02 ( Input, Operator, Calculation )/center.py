6e4fc0cc018655471d38270f3695d50932055fb8
PriontoAbdullah <priontosoft@gmail.com>
2020-07-04 22:48:46 +0600

    chepter 2 complete

---
+++ Chepter 02 ( Input, Operator, Calculation )/center.py
@@ -0,0 +1,5 @@
+name='prionto'
+print(name.center(8,'*'))
+print(name.center(11,'^'))
+name=input("Enter your name: ")
+print(name.center(len(name)+4,"#"))
---
+++ Chepter 02 ( Input, Operator, Calculation )/replace & find.py
@@ -0,0 +1,5 @@
+love = 'She is my beautyful wife & she is also my cute girlfriend.'
+print(love.replace(' ','_'))
+print(love.replace('is','Was',1))
+pos=love.find('is')
+print(love.find('is',pos+1))
